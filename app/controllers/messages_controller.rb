c429367662f50e3e60b67eb51e9802c91175bb9f
Patrick Minero <patrickminerovalencia@gmail.com>
2022-02-15 21:24:06 +0100

    added message show view and controller method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,15 @@ class MessagesController < ApplicationController
     @message ||= Message.new
   end
 
+  def show
+    @message = Message.find(params[:id])
+  end
+
   def create
     @message = Message.new(message_params)
     if @message.save
       send_email ContactMailer, :invite, contact: @message
-      redirect_to new_message_path(@message), alert: ['Message sent correctly']
+      redirect_to message_path(@message)
     else
       redirect_to new_message_path(@message), alert: @message.errors.full_messages
     end
