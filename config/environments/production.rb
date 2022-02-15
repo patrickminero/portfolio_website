9299c3de394011f676496120682b68ef2f4e04e8
Patrick Minero <patrickminerovalencia@gmail.com>
2022-02-15 21:11:23 +0100

    added config for active mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "personal_website_production"
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "patminero.com" }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
